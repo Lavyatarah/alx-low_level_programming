ee8a900430560b226f56f27617e3a26bea05b899
Lavyatrah <mattlavyy@gmail.com>
2023-02-22 07:43:27 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,27 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
 #include "main.h"
 
 /**
- * main - entry block
- * no argument
- * Return: 0
- **/
-
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char ermias[] = "_putchar";
-
-	int c;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(ermias[c]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
