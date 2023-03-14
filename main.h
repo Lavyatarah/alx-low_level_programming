359363d97bcd4705201abc1271674cf0c6b9721b
lavyatarah <mattlavyy@gmail.com>
2023-03-14 02:00:44 -0700

    0-create_array.c

--- main.h
+++ main.h
@@ -1,13 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
+/**
+ * File: main
+ *
+ * Desc: header file
+ */
 int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
-#endif
+# endif
