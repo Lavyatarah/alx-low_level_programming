32582da42b6425446fc44087d06b6d48f430d96e
lavyatarah <mattlavyy@gmail.com>
2023-06-26 09:00:09 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Lavyatarah/alx-low_level_programming/blob/master/0x18-dynamic_libraries/putshack.so
+wget -P /tmp/ https://github.com/Lavyatarah/alx-low_level_programming/blob/master/0x18-dynamic_libraries/putshack.o
 export LD_PREALOAD =/tmp/putshack.so
